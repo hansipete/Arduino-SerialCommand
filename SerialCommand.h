2c0f9c90425b5c78ca11a8acae26b8a365ff856c
Hans Kadel <hans.kadel@ixds.com>
2017-12-05 23:49:26 +0100

    use serial1 now. no usb working anymore on linkits

--- SerialCommand.h
+++ SerialCommand.h
@@ -35,7 +35,7 @@
 
 // which serial port to use
 // TODO: add Stream &referencedport to init in .cpp
-#define SerialPort Serial
+#define SerialPort Serial1
 
 // Size of the input buffer in bytes (maximum length of one command plus arguments)
 #define SERIALCOMMAND_BUFFER 32
